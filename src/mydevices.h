1e2381236ffc4cd6fea4ebef319244807e614963
Julien <julienkinderf@yahoo.com>
2020-05-20 11:09:34 +0200

    merge test julien

--- src/mydevices.h
+++ src/mydevices.h
@@ -8,6 +8,15 @@
 #include "core_simulation.h"
 #include <fstream>
 
+class Sensor : public Device
+{
+private:
+    int delay;
+    std::string file;
 
+public:
+    Sensor(int delay, std::string file);
+    virtual void valueUpdate() = 0;
+};
 
 #endif
