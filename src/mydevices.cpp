9b7f96dc49741b52f55611e6aa55b45dbdc7f353
Guillaume-Vincent <g_vincen@etud.insa-toulouse.fr>
2020-05-20 12:09:13 +0200

    Merge successful (finally)

--- src/mydevices.cpp
+++ src/mydevices.cpp
@@ -58,3 +58,52 @@ void DigitalSensor::run()
         sleep(delay);
     }
 }
+
+DigitalActuator::DigitalActuator(int delay)
+: delay(delay), state(LOW){
+
+}
+
+Buzzer::Buzzer(int delay, int frequency)
+: DigitalActuator(delay), frequency(frequency){
+
+}
+
+void Buzzer::run(){
+    bool oldstate = LOW;
+    while (1){
+        if (ptrmem != NULL)
+            state = *ptrmem;
+        if (oldstate != state)
+        {
+            if (state == LOW)
+                cout << "/!\\ ALARM OFF /!\\" << endl;
+            else
+                cout << "/!\\ ALARM ON /!\\" << endl;
+            oldstate = state;
+        }
+        sleep(delay);
+    }
+}
+
+LED::LED(int delay, std::string color)
+: DigitalActuator(delay), color(color){
+
+}
+
+void LED::run(){
+    bool oldstate = LOW;
+    while (1){
+        if (ptrmem != NULL)
+            state = *ptrmem;
+        if (oldstate != state)
+        {
+            if (state == LOW)
+                cout << "LED OFF" << endl;
+            else
+                cout << "LED ON" << endl;
+            oldstate = state;
+        }
+        sleep(delay);
+    }
+}
