61a855cd139e05142e155904b10d76237d44a8e9
Guillaume-Vincent <g_vincen@etud.insa-toulouse.fr>
2020-05-20 11:00:02 +0200

    Cleared fire alarm project

--- src/board.cpp
+++ src/board.cpp
@@ -2,25 +2,15 @@
 #include "mydevices.h"
 
 extern int luminosite_environnement;
-int main()
-{
-  // creation d'une board
-  Board esp8266;
-  // achat des senseurs et actionneurs
-  AnalogSensorTemperature temperature(DELAY, TEMP);
-  IntelligentDigitalActuatorLED led1(0.1);
-  I2CActuatorScreen screen;
-  AnalogSensorLight luminosite(DELAY);
-  ExternalDigitalSensorButton bouton(0.1);
+int main() {
+    // creation d'une board
+    Board esp8266;
+    // achat des senseurs et actionneurs
 
-  // branchement des capteurs actionneurs
-  esp8266.pin(4, bouton);
-  esp8266.pin(2, luminosite);
-  esp8266.pin(1, temperature);
-  esp8266.pin(0, led1);
-  esp8266.i2c(1, screen);
+    // branchement des capteurs actionneurs
 
-  // allumage de la carte
-  esp8266.run();
-  return 0;
+
+    // allumage de la carte
+    esp8266.run();
+    return 0;
 }
