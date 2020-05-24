74afe17315a3630e98b6beff28cc4d7866e24a3e
Julien <julienkinderf@yahoo.com>
2020-05-24 21:15:32 +0200

    fixed }

--- src/mydevices.cpp
+++ src/mydevices.cpp
@@ -22,85 +22,86 @@ void AnalogSensor::update()
         infile >> buffer;
         value = std::stof(buffer);
     }
+}
 
-    void AnalogSensor::run()
+void AnalogSensor::run()
+{
+    while (1)
     {
-        while (1)
+        update();
+        if (ptrmem != NULL)
+            *ptrmem = value;
+        sleep(delay);
+    }
+}
+
+DigitalSensor::DigitalSensor(int delay, std::string file, std::string name)
+    : Sensor(delay, file, name), state(LOW) {}
+
+// Checks if the file exists
+// Change the state to true if it does, false if not
+void DigitalSensor::update()
+{
+    if (ifstream(this->file))
+        state = true;
+    else
+        state = false;
+}
+
+void DigitalSensor::run()
+{
+    while (1)
+    {
+        update();
+        if (ptrmem != NULL)
+            *ptrmem = state;
+        sleep(delay);
+    }
+}
+
+DigitalActuator::DigitalActuator(int delay, std::string name)
+    : delay(delay), state(LOW), name(name) {}
+
+Buzzer::Buzzer(int delay, int frequency, std::string name)
+    : DigitalActuator(delay, name), frequency(frequency) {}
+
+void Buzzer::run()
+{
+    bool oldstate = LOW;
+    while (1)
+    {
+        if (ptrmem != NULL)
+            state = *ptrmem;
+        if (oldstate != state)
         {
-            update();
-            if (ptrmem != NULL)
-                *ptrmem = value;
-            sleep(delay);
+            if (state == LOW)
+                cout << "/!\\ ALARM OFF /!\\" << endl;
+            else
+                cout << "/!\\ ALARM ON /!\\" << endl;
+            oldstate = state;
         }
+        sleep(delay);
     }
+}
 
-    DigitalSensor::DigitalSensor(int delay, std::string file, std::string name)
-        : Sensor(delay, file, name), state(LOW) {}
+LED::LED(int delay, std::string color, std::string name)
+    : DigitalActuator(delay, name), color(color) {}
 
-    // Checks if the file exists
-    // Change the state to true if it does, false if not
-    void DigitalSensor::update()
+void LED::run()
+{
+    bool oldstate = LOW;
+    while (1)
     {
-        if (ifstream(this->file))
-            state = true;
-        else
-            state = false;
-    }
-
-    void DigitalSensor::run()
-    {
-        while (1)
+        if (ptrmem != NULL)
+            state = *ptrmem;
+        if (oldstate != state)
         {
-            update();
-            if (ptrmem != NULL)
-                *ptrmem = state;
-            sleep(delay);
+            if (state == LOW)
+                cout << "LED OFF" << endl;
+            else
+                cout << "LED ON" << endl;
+            oldstate = state;
         }
+        sleep(delay);
     }
-
-    DigitalActuator::DigitalActuator(int delay, std::string name)
-        : delay(delay), state(LOW), name(name) {}
-
-    Buzzer::Buzzer(int delay, int frequency, std::string name)
-        : DigitalActuator(delay, name), frequency(frequency) {}
-
-    void Buzzer::run()
-    {
-        bool oldstate = LOW;
-        while (1)
-        {
-            if (ptrmem != NULL)
-                state = *ptrmem;
-            if (oldstate != state)
-            {
-                if (state == LOW)
-                    cout << "/!\\ ALARM OFF /!\\" << endl;
-                else
-                    cout << "/!\\ ALARM ON /!\\" << endl;
-                oldstate = state;
-            }
-            sleep(delay);
-        }
-    }
-
-    LED::LED(int delay, std::string color, std::string name)
-        : DigitalActuator(delay, name), color(color) {}
-
-    void LED::run()
-    {
-        bool oldstate = LOW;
-        while (1)
-        {
-            if (ptrmem != NULL)
-                state = *ptrmem;
-            if (oldstate != state)
-            {
-                if (state == LOW)
-                    cout << "LED OFF" << endl;
-                else
-                    cout << "LED ON" << endl;
-                oldstate = state;
-            }
-            sleep(delay);
-        }
-    }
+}
