db7da95c62acd8f7e362d85629a2f2718fcd025a
Julien <julienkinderf@yahoo.com>
2020-05-24 21:13:37 +0200

    plugging devices

--- src/board.cpp
+++ src/board.cpp
@@ -2,13 +2,37 @@
 #include "mydevices.h"
 
 extern int luminosite_environnement;
-int main() {
+int main()
+{
     // creation d'une board
     Board esp8266;
+
     // achat des senseurs et actionneurs
+    //TODO : exceptions si pas de fichier avec ce nom (si pas de data, renvoie -1)
+    AnalogSensor sensor_temperature(DELAY, "temp.data", "temperature");
+    AnalogSensor sensor_smoke(DELAY, "smoke.data", "smoke");
+    AnalogSensor sensor_battery(DELAY, "battery.data", "battery");
+
+    // les boutons
+    DigitalSensor button_test(DELAY, "button_test", "test button");
+    DigitalSensor button_reset(DELAY, "button_reset", "reset button");
+
+    // les LEDs
+    LED led_alarm_state(DELAY, "red", "alarm state");
+    LED led_low_battery(DELAY, "orange", "low battery");
+
+    // le buzzer
+    Buzzer buzzer(DELAY, 5, "buzzer");
 
     // branchement des capteurs actionneurs
-
+    esp8266.pin(0, sensor_temperature);
+    esp8266.pin(1, sensor_smoke);
+    esp8266.pin(2, sensor_battery);
+    esp8266.pin(3, button_test);
+    esp8266.pin(4, button_reset);
+    esp8266.pin(5, led_alarm_state);
+    esp8266.pin(6, led_low_battery);
+    esp8266.pin(7, buzzer);
 
     // allumage de la carte
     esp8266.run();
--- src/mydevices.cpp
+++ src/mydevices.cpp
@@ -22,82 +22,85 @@ void AnalogSensor::update()
         infile >> buffer;
         value = std::stof(buffer);
     }
-}
 
-void AnalogSensor::run()
-{
-    while (1)
+    void AnalogSensor::run()
     {
-        update();
-        if (ptrmem != NULL)
-            *ptrmem = value;
-        sleep(delay);
+        while (1)
+        {
+            update();
+            if (ptrmem != NULL)
+                *ptrmem = value;
+            sleep(delay);
+        }
     }
-}
 
-DigitalSensor::DigitalSensor(int delay, std::string file, std::string name)
-    : Sensor(delay, file, name), state(LOW) {}
+    DigitalSensor::DigitalSensor(int delay, std::string file, std::string name)
+        : Sensor(delay, file, name), state(LOW) {}
 
-// Checks if the file exists
-// Change the state to true if it does, false if not
-void DigitalSensor::update()
-{
-    if (ifstream(this->file))
-        state = true;
-    else
-        state = false;
-}
-
-void DigitalSensor::run()
-{
-    while (1)
+    // Checks if the file exists
+    // Change the state to true if it does, false if not
+    void DigitalSensor::update()
     {
-        update();
-        if (ptrmem != NULL)
-            *ptrmem = state;
-        sleep(delay);
+        if (ifstream(this->file))
+            state = true;
+        else
+            state = false;
     }
-}
 
-DigitalActuator::DigitalActuator(int delay, std::string name)
-: delay(delay), state(LOW), name(name){}
-
-Buzzer::Buzzer(int delay, int frequency, std::string name)
-: DigitalActuator(delay, name), frequency(frequency) {}
-
-void Buzzer::run(){
-    bool oldstate = LOW;
-    while (1){
-        if (ptrmem != NULL)
-            state = *ptrmem;
-        if (oldstate != state)
+    void DigitalSensor::run()
+    {
+        while (1)
         {
-            if (state == LOW)
-                cout << "/!\\ ALARM OFF /!\\" << endl;
-            else
-                cout << "/!\\ ALARM ON /!\\" << endl;
-            oldstate = state;
+            update();
+            if (ptrmem != NULL)
+                *ptrmem = state;
+            sleep(delay);
         }
-        sleep(delay);
     }
-}
 
-LED::LED(int delay, std::string color, std::string name)
-: DigitalActuator(delay, name), color(color) {}
+    DigitalActuator::DigitalActuator(int delay, std::string name)
+        : delay(delay), state(LOW), name(name) {}
 
-void LED::run(){
-    bool oldstate = LOW;
-    while (1){
-        if (ptrmem != NULL)
-            state = *ptrmem;
-        if (oldstate != state)
+    Buzzer::Buzzer(int delay, int frequency, std::string name)
+        : DigitalActuator(delay, name), frequency(frequency) {}
+
+    void Buzzer::run()
+    {
+        bool oldstate = LOW;
+        while (1)
         {
-            if (state == LOW)
-                cout << "LED OFF" << endl;
-            else
-                cout << "LED ON" << endl;
-            oldstate = state;
+            if (ptrmem != NULL)
+                state = *ptrmem;
+            if (oldstate != state)
+            {
+                if (state == LOW)
+                    cout << "/!\\ ALARM OFF /!\\" << endl;
+                else
+                    cout << "/!\\ ALARM ON /!\\" << endl;
+                oldstate = state;
+            }
+            sleep(delay);
         }
-        sleep(delay);
     }
-}
+
+    LED::LED(int delay, std::string color, std::string name)
+        : DigitalActuator(delay, name), color(color) {}
+
+    void LED::run()
+    {
+        bool oldstate = LOW;
+        while (1)
+        {
+            if (ptrmem != NULL)
+                state = *ptrmem;
+            if (oldstate != state)
+            {
+                if (state == LOW)
+                    cout << "LED OFF" << endl;
+                else
+                    cout << "LED ON" << endl;
+                oldstate = state;
+            }
+            sleep(delay);
+        }
+    }
