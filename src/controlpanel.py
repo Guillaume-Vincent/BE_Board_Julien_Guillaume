aaaddc138890f5cc973a1d21612e6271593eef00
Julien <julienkinderf@yahoo.com>
2020-05-25 15:41:41 +0200

    added control panel for the buttons

---
+++ src/controlpanel.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import tkinter
+import os
+
+# get the path to this script
+dirname = os.path.dirname(os.path.realpath(__file__))
+
+path_arm = dirname + "/button/arm.button"
+path_test = dirname + "/button/test.button"
+path_reset = dirname + "/button/reset.button"
+
+
+def toggleFile(path):
+    """Toggles the presence of the corresponding file"""
+    print(f"toggling {path}")
+    if (os.path.isfile(path)):
+        os.remove(path)
+    else:
+        open(path, 'a').close()
+
+
+win = tkinter.Tk()
+button_arm = tkinter.Button(win, text="Arm button",
+                            command=lambda: toggleFile(path_arm))
+button_test = tkinter.Button(win, text="Test button",
+                             command=lambda: toggleFile(path_test))
+button_reset = tkinter.Button(win, text="Reset button",
+                              command=lambda: toggleFile(path_reset))
+button_arm.pack()
+button_test.pack()
+button_reset.pack()
+win.mainloop()
